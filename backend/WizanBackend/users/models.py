1c42c47f2a1a53d7c8371063287c1a3c9cb9c605
Ahmed Sami <ahmedsamy121277@gmail.com>
2026-06-05 21:45:30 +0300

    fix user model

--- backend/WizanBackend/users/models.py
+++ backend/WizanBackend/users/models.py
@@ -6,11 +6,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class User(AbstractUser):
