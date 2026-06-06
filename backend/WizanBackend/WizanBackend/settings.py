1a56d921fbf200fe39a615186c9869579f892cb0
Ahmed Sami <ahmedsamy121277@gmail.com>
2026-06-06 20:59:51 +0300

    link database

--- backend/WizanBackend/WizanBackend/settings.py
+++ backend/WizanBackend/WizanBackend/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'WizanBackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'wizan_db'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
     }
 }
 
